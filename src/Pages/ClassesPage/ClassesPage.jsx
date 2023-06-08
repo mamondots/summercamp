e69eaf087e1ddb0cab21303bf0fe03ff76bd8775
mamondots <shikderr112@gmail.com>
2023-06-08 20:50:20 +0600

    adding instructor or classes page with fake data

--- src/Pages/ClassesPage/ClassesPage.jsx
+++ src/Pages/ClassesPage/ClassesPage.jsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from "react";
+import ClassesPageCard from "./ClassesPageCard";
 
 
 const ClassesPage = () => {
+    const [classItems,setClassItems] = useState([])
+    useEffect(() =>{
+        fetch('classes.json')
+        .then(res => res.json())
+        .then(data => {
+            setClassItems(data)
+            console.log(data)
+        })
+    },[])
     return (
-        <div className="max-w-[2520px] mx-auto xl:px-20 md:px-10 px-4 pt-40 ">
-            <h2>this is classNamepage</h2>
+        <div className="max-w-[2520px] mx-auto xl:px-20 md:px-10 px-4 pt-28 pb-12 ">
+            <div className="bg-[#570DF8] p-12">
+                <p className="text-white">Here All Classes</p>
+                <h2 className="text-white text-3xl font-bold pt-1">Join Your Favarit Class</h2>
+            </div>
+            <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-2 lg:gap-8 md:gap-6 sm:gap-2 gap-2 pt-12">
+                 
+                     {
+                        classItems.map((classitem, index) => <ClassesPageCard
+                        key={classitem.index}
+                        classitem={classitem}
+                        >
+ 
+                        </ClassesPageCard>) 
+                     }
+                 
+            </div>
         </div>
     );
 };
--- src/Pages/InstructorsPage/InstructorsPage.jsx
+++ src/Pages/InstructorsPage/InstructorsPage.jsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
+import InstructorsPageCard from "./InstructorsPageCard";
 
 
 const InstructorsPage = () => {
+    const [instructors ,setInstructors] = useState([])
+    useEffect(() =>{
+        fetch('Instructor.json')
+        .then(res => res.json())
+        .then(data => setInstructors(data))
+    },[])
     return (
-        <div className="max-w-[2520px] mx-auto xl:px-20 md:px-10 px-4 pt-40 ">
-            <h2>this is instaick page</h2>
+        <div className="max-w-[2520px] mx-auto xl:px-20 md:px-10 px-4 pt-28 pb-2 ">
+            <div className="bg-[#570DF8] p-12">
+                <p className="text-white">Here All Instructors</p>
+                <h2 className="text-white text-3xl font-bold pt-1">Join With Your Favarit Instructors</h2>
+            </div>
+
+            <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 lg:gap-8 md:gap-6 sm:gap-2 gap-2 pt-12">
+                 
+            {
+                      instructors.map((instructor,index) =><InstructorsPageCard
+                      key={instructor.index}
+                        instructor={instructor}
+                      >
+
+                      </InstructorsPageCard>)  
+                    } 
+                 
+            </div>
+
         </div>
     );
 };
